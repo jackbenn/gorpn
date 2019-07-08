9645a261c13341d6b15447c5f72e698d6944c882
jackbenn <jack@bennetto.com>
2019-07-07 18:32:47 -0700

    make type; create pop/push methods

--- gorpn.go
+++ gorpn.go
@@ -10,44 +10,59 @@ import (
 	"strings"
 )
 
-const max_size = 100
+type RPN struct {
+	stack []float64
+}
+
+func (rpn *RPN) push(x float64) {
+	fmt.Println(append(rpn.stack, x))
+	rpn.stack = append(rpn.stack, x)
+}
+
+func (rpn *RPN) pop() float64 {
+	x := rpn.stack[len(rpn.stack)-1]
+	rpn.stack = rpn.stack[:len(rpn.stack)-1]
+	return x
+}
+
+const maxSize = 100
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("RPN calculator")
 	fmt.Println("Enter a bunch of symbols")
 	// when I understand slices better I can do this better
-	var stack []float64
+	var stack RPN
 	repeat := true
 	for repeat {
 		fmt.Print("] ")
 		text, _ := reader.ReadString('\n')
 		tokens := strings.Fields(text)
 
-		is_number := regexp.MustCompile(`^\d*\.?\d*$`)
+		isNumber := regexp.MustCompile(`^\d*\.?\d*$`)
 		for i := 0; i < len(tokens); i += 1 {
 			token := tokens[i]
 
 			if token == "" {
 				continue
-			} else if is_number.MatchString(token) {
+			} else if isNumber.MatchString(token) {
 				// should I do this through error handling?
+				fmt.Println("found a number")
 				number, _ := strconv.ParseFloat(token, 64)
-				stack = append(stack, number)
+				stack.push(number)
 			} else if token == "+" {
-				a := stack[len(stack)-2]
-				b := stack[len(stack)-1]
-				stack = append(stack[:len(stack)-2], a+b)
+				b := stack.pop()
+				a := stack.pop()
+				stack.push(a + b)
 			} else if token == "-" {
-				a := stack[len(stack)-2]
-				b := stack[len(stack)-1]
-				stack = append(stack[:len(stack)-2], a-b)
+				b := stack.pop()
+				a := stack.pop()
+				stack.push(a - b)
 			} else if strings.HasPrefix(token, "q") {
 				fmt.Println("exiting")
 				repeat = false
 				break
 			}
-			fmt.Println(token[0])
 
 			fmt.Println(stack)
 
