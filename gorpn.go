ea19e542598ff6033a4dc648885eaac234500805
jackbenn <jack@bennetto.com>
2019-07-07 22:30:40 -0700

    move parse code to methods

--- gorpn.go
+++ gorpn.go
@@ -10,12 +10,15 @@ import (
 	"strings"
 )
 
+var isNumber = regexp.MustCompile(`^\d*\.?\d*$`)
+
+const maxSize = 100
+
 type RPN struct {
 	stack []float64
 }
 
 func (rpn *RPN) push(x float64) {
-	fmt.Println(append(rpn.stack, x))
 	rpn.stack = append(rpn.stack, x)
 }
 
@@ -25,48 +28,53 @@ func (rpn *RPN) pop() float64 {
 	return x
 }
 
-const maxSize = 100
+func (rpn *RPN) parseToken(token string) {
+	if token == "" {
+		return
+	} else if isNumber.MatchString(token) {
+		// should I do this through error handling?
+		number, _ := strconv.ParseFloat(token, 64)
+		rpn.push(number)
+	} else if token == "+" {
+		b := rpn.pop()
+		a := rpn.pop()
+		rpn.push(a + b)
+	} else if token == "-" {
+		b := rpn.pop()
+		a := rpn.pop()
+		rpn.push(a - b)
+	}
+}
+
+func (rpn *RPN) parseTokens(text string) bool {
+	tokens := strings.Fields(text)
+
+	for i := 0; i < len(tokens); i += 1 {
+		token := tokens[i]
+
+		if strings.HasPrefix(token, "q") {
+			fmt.Println("exiting")
+			return false
+		} else {
+			rpn.parseToken(token)
+		}
+		fmt.Println(rpn)
+	}
+	return true
+
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("RPN calculator")
 	fmt.Println("Enter a bunch of symbols")
 	// when I understand slices better I can do this better
-	var stack RPN
+	var rpn RPN
 	repeat := true
 	for repeat {
 		fmt.Print("] ")
 		text, _ := reader.ReadString('\n')
-		tokens := strings.Fields(text)
-
-		isNumber := regexp.MustCompile(`^\d*\.?\d*$`)
-		for i := 0; i < len(tokens); i += 1 {
-			token := tokens[i]
-
-			if token == "" {
-				continue
-			} else if isNumber.MatchString(token) {
-				// should I do this through error handling?
-				fmt.Println("found a number")
-				number, _ := strconv.ParseFloat(token, 64)
-				stack.push(number)
-			} else if token == "+" {
-				b := stack.pop()
-				a := stack.pop()
-				stack.push(a + b)
-			} else if token == "-" {
-				b := stack.pop()
-				a := stack.pop()
-				stack.push(a - b)
-			} else if strings.HasPrefix(token, "q") {
-				fmt.Println("exiting")
-				repeat = false
-				break
-			}
-
-			fmt.Println(stack)
-
-		}
+		repeat = rpn.parseTokens(text)
 	}
 
 }
